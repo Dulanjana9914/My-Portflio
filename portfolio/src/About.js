5ebf7a9325d524d386eb94bf72251b0580a6d0b4
Dulanjana Lakshan <it20265892@my.sliit.lk>
2022-12-28 20:48:49 +0530

    CV edited

--- portfolio/src/About.js
+++ portfolio/src/About.js
@@ -112,7 +112,7 @@ const About = () => {
               <br />
 
               <div className="about__button d__flex align__items__center">
-                <a href="https://drive.google.com/file/d/1SZVBTM6JJzcGgkN63E9iKdFw1ad_3utI/view?usp=sharing">
+                <a href="https://drive.google.com/file/d/1SZVBTM6JJzcGgkN63E9iKdFw1ad_3utI/view?usp=share_link">
                   <button className="about btn pointer">Download CV</button>
                 </a>
                 <a href="https://medium.com/@it20265892">
